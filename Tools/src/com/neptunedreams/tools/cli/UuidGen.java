52097ba72d44f761631f5af57d4bed5ceda09a6d
Miguel Muñoz <SwingGuy1024@yahoo.com>
2019-03-06 14:06:53 -0800

    Update UuidGen.java

--- Tools/src/com/neptunedreams/tools/cli/UuidGen.java
+++ Tools/src/com/neptunedreams/tools/cli/UuidGen.java
@@ -1,5 +1,8 @@
 package com.neptunedreams.tools.cli;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.util.UUID;
 
 /**
@@ -10,7 +13,45 @@ public final class UuidGen {
     private UuidGen() { }
 
     public static void main(String[] args) {
+        UuidGen uuidGen = processArgs(args);
         UUID uuid = UUID.randomUUID();
-        System.out.println(uuid);
+        if (uuidGen.useClipboard) {
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            StringSelection stringSelection = new StringSelection(uuid.toString());
+            clipboard.setContents(stringSelection, stringSelection);
+            System.out.println(uuid + " -> Clipboard");
+        } else {
+            System.out.println(uuid);
+        }
     }
+
+    private static UuidGen processArgs(String[] args) {
+        UuidGen uuidGen = new UuidGen();
+        if (args.length > 0) {
+            if (args[0].contains("h")) {
+                System.out.println("Usage: uuid        Print out a new UUID.\n");
+                System.out.println("       uuid -c     Print out a new UUID and copy it to the clipboard.");
+                System.out.println("       uuid c\n");
+                System.out.println("       uuid --help  Print out this text and exit.");
+                System.out.println("       uuid --h");
+                System.out.println("       uuid -help");
+                System.out.println("       uuid -h");
+                System.out.println("       uuid help");
+                System.out.println("       uuid h");
+                System.out.println("\nTo install, add this to your ~/.bash_profile:");
+                System.out.println("alias uuid=\"java -cp ~/Tools/out/production/Tools com.neptunedreams.tools.cli.UuidGen\"");
+                System.exit(0);
+            }
+            String[] variations = { "c", "-c", "--c" };
+            for (String v: variations) {
+                if (v.equalsIgnoreCase(args[0])) {
+                    uuidGen.useClipboard = true;
+                    break;
+                }
+            }
+        }
+        return uuidGen;
+    }
+
+    private boolean useClipboard = false;
 }
