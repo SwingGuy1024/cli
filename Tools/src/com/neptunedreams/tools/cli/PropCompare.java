85d7835fa2f41e1ca3ad5f935709d80555f3da35
Miguel Munoz <mmunoz@tillster.com>
2020-02-05 15:44:53 -0800

    Stopped Using Maps. Trailing Space support.

--- Tools/src/com/neptunedreams/tools/cli/PropCompare.java
+++ Tools/src/com/neptunedreams/tools/cli/PropCompare.java
@@ -1,13 +1,12 @@
 package com.neptunedreams.tools.cli;
 
 import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.TreeSet;
@@ -16,6 +15,9 @@ import java.util.TreeSet;
  * Compares contents of two properties files, or of one properties file with those on the clipboard.
  */
 public class PropCompare {
+
+  public static final char STAR = '*';
+
   public static void main(String[] args) throws IOException, UnsupportedFlavorException {
     switch (args.length) {
       case 1:
@@ -35,9 +37,9 @@ public class PropCompare {
 
   private static Properties getPropertiesFromClipboard() throws UnsupportedFlavorException, IOException {
     Properties cbProps = new Properties();
-    String txt = Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor).toString();
-    StringReader clipReader = new StringReader(txt);
-    cbProps.load(clipReader);
+    Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    String txt = systemClipboard.getData(DataFlavor.stringFlavor).toString();
+    cbProps.load(new StringReader(txt));
     return cbProps;
   }
 
@@ -54,35 +56,46 @@ public class PropCompare {
   }
   
   private static void compare(Properties left, Properties right) {
-    Map<String, String> leftMap = copyToMap(left);
-    Map<String, String> rightMap = copyToMap(right);
-    Set<String> propsMissingFromLeft = getMissing(leftMap, rightMap);
-    Set<String> propsMissingFromRight = getMissing(rightMap, leftMap);
-    System.out.printf("Properties missing from left: %d/%d %n", propsMissingFromLeft.size(), rightMap.size());
-    showProperties(propsMissingFromLeft, rightMap);
+    Set<String> propsMissingFromLeft = getMissing(left, right);
+    Set<String> propsMissingFromRight = getMissing(right, left);
+    System.out.printf("Properties missing from left: %d/%d %n", propsMissingFromLeft.size(), right.size());
+    showProperties(propsMissingFromLeft, right);
 
-    System.out.printf("%nProperties missing from right: %d/%d %n", propsMissingFromRight.size(), leftMap.size());
-    showProperties(propsMissingFromRight, leftMap);
+    System.out.printf("%nProperties missing from right: %d/%d %n", propsMissingFromRight.size(), left.size());
+    showProperties(propsMissingFromRight, left);
     
-    Set<String> commonKeys = getCommonKeys(leftMap, rightMap);
-    System.out.printf("%nChanged Values:%n");
+    Set<String> commonKeys = getCommonKeys(left, right);
+    System.out.printf("%nMismatches:%n");
     int count = 0;
+    boolean trailingSpace = false;
     for (String key: commonKeys) {
-      String leftValue = leftMap.get(key);
-      String rightValue = rightMap.get(key);
+      String leftValue = left.getProperty(key);
+      String rightValue = right.getProperty(key);
+      char leftTrailChar = trailingSpaceMark(leftValue);
+      char rightTrailChar = trailingSpaceMark(rightValue);
+      if (!trailingSpace && (leftTrailChar == STAR || rightTrailChar == STAR)) {
+        trailingSpace = true;
+      }
+      
       if (!leftValue.equals(rightValue)) {
-        System.out.printf("%n%s%n", key);
-        System.out.printf("<  =%s%n", leftValue);
-        System.out.printf(">  =%s%n", rightValue);
+        System.out.printf("<%c %s=%s%n", leftTrailChar, key, leftValue);
+        System.out.printf(">%c %s=%s%n%n", rightTrailChar, key, rightValue);
         count++;
       }
     }
     System.out.printf("Total of %d mismatches%n", count);
+    if (trailingSpace) {
+      System.out.printf("%c Has one or more trailing spaces%n", STAR);
+    }
   }
   
-  private static Set<String> getCommonKeys(Map<String, String> leftMap, Map<String, String> rightMap) {
+  private static char trailingSpaceMark(String s) {
+    return s.equals(s.trim())? ' ' : STAR;
+  }
+
+  private static Set<String> getCommonKeys(Properties leftMap, Properties rightMap) {
     Set<String> commonKeys = new TreeSet<>();
-    for (String key: leftMap.keySet()) {
+    for (String key: leftMap.stringPropertyNames()) {
       if (rightMap.containsKey(key)) {
         commonKeys.add(key);
       }
@@ -90,23 +103,15 @@ public class PropCompare {
     return commonKeys;
   }
   
-  private static void showProperties(Set<String> missing, Map<String, String> map) {
+  private static void showProperties(Set<String> missing, Properties map) {
     for (String key: missing) {
-      System.out.printf("%s=%s%n", key, map.get(key));
+      System.out.printf("%s=%s%n", key, map.getProperty(key));
     }
   }
   
-  private static Map<String, String> copyToMap(Properties props) {
-    Map<String, String> map = new HashMap<>();
-    for (String pName: props.stringPropertyNames()) {
-      map.put(pName, props.getProperty(pName));
-    }
-    return map;
-  }
-  
-  private static Set<String> getMissing(Map<String, String> from, Map<String, String> source) {
+  private static Set<String> getMissing(Properties from, Properties source) {
     Set<String> missingKeySet = new TreeSet<>();
-    for (String key: source.keySet()) {
+    for (String key: source.stringPropertyNames()) {
       if (!from.containsKey(key)) {
         missingKeySet.add(key);
       }
