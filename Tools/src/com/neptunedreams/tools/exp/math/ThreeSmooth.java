62cbbc9bb83c5408bee30871a2dba13d02b522b3
SwingGuy1024 <SwingGuy1024@yahoo.com>
2023-09-30 17:29:45 -0700

    Javadoc fixes Changed name of List<Long> columns to rows

--- Tools/src/com/neptunedreams/tools/exp/math/ThreeSmooth.java
+++ Tools/src/com/neptunedreams/tools/exp/math/ThreeSmooth.java
@@ -10,69 +10,72 @@ import java.util.List;
  * horizontally along the table raises the exponent of 2 by one, and moving vertically, does that for the
  * exponent of 3:</p>
  * <pre>
- * 
+ *
+ * <b>\u2193 Exponent of 3</b>
  *     <b>0    1    2    3    4    5  \u2190 Exponent of 2</b> 
  * <b>0</b>   1    2    4    8   16   32 ...
  * <b>1</b>   3    6   12   24   48   96
  * <b>2</b>   9   18   36   72  144  288
  * <b>3</b>  27   54  108  216  432  864
  * <b>4</b>  81  162  324  648 1296 2592
- * <b>\u2191 Exponent of 3</b>
  * </pre>
  * <p>As you can see, each element in the table is double the element to its left, and triple the element above it.</p>
  * <p>After finding a few numbers in the sequence, here's the procedure to find the next on. For each row, you look at
- * the element to the right of the last one used. You do this for every column, starting from the top, until you reach
- * a column that gives you its first element. You return the smallest number you find. So, say the last one you found
+ * the element to the right of the last one used. You do this for every row, starting from the top, until you reach
+ * a row that gives you its first element. You return the smallest number you find. So, say the last one you found
  * was 12. You consider the 16 from row 1, the 24 from row 2, the 18 from row three, and the 27 from row 4. You stop
  * there, because 27 is the first element of its row. Of those four numbers, the lowest is 16, so that's what you
  * return. The next time you look, you'll include the 32 from the first row, but none of the others change.</p>
- * <p>I practice, I don't generate the entire table, I just keep a single column, called {@code columns}, which
- * includes the "next" element from its row. So after finding 12, {@code columns} would have these elements:
+ * <p>I practice, I don't generate the entire table, I just keep a single List, called {@code rows}, which
+ * includes the "next" element from its row. So after finding 12, {@code rows} would have these elements:
  * { 16, 24, 18, 27 }. This return the lowest one, which is 16. Then it doubles that value and replace it in the table,
- * so {@code columns} becomes { 32, 24, 18, 27 }. If it needs to replace the last element, it first triples it and
- * appends it to {@code columns}. So if the last number it returned was 24, the List will have these four elements:
- * { 32, 48, 36, 27 }. Then, after returning the lowest element, which is 27, {@code columns} will now contain five
+ * so {@code rows} becomes { 32, 24, 18, 27 }. If it needs to replace the last element, it first triples it and
+ * appends it to {@code rows}. So if the last number it returned was 24, the List will have these four elements:
+ * { 32, 48, 36, 27 }. Then, after returning the lowest element, which is 27, {@code rows} will now contain five
  * elements: { 32, 48, 36, 54, 81 }.</p>
- * <p>To begin after one million, start the generator with 2<sup>12</sup>3<sup>5</sup>. This produces the last
- * threeSmooth number before 1,000,000, which is 995328. The {@code columns} list will have 13 values</p>
+ * <p>The number of elements in {@code rows} will be Log<sub>3</sub>(R), where R is the last result returned. So, to
+ * begin after one million, start the generator with 2<sup>12</sup>3<sup>5</sup>. This produces the last
+ * threeSmooth number before 1,000,000, which is 995328. Log<sub>3</sub>(1,000,000) is 12.58… so the {@code rows} list
+ * will have 13 values.</p>
  * <p>To do: Add a limit member</p>
  * <p>Created by IntelliJ IDEA.
  * <p>Date: 9/26/23
  * <p>Time: 12:04 PM
  *
- * @author Miguel Mu\u00f1oz
+ * @author Miguel Muñoz
  */
 public final class ThreeSmooth implements Iterator<Long> {
   /**
    * Create a ThreeSmooth iterator that starts from zero.
    */
   private ThreeSmooth() {
-    columns.add(1L);
+    rows.add(1L);
   }
 
   /**
    * Create a ThreeSmooth iterator that starts with the specified value, or the first threeSmooth number after that value.
-   * @param start
+   * @param start Starting ThreeSmooth number. The first number returned by the iterator will be the lowest threeSmooth
+   *              number greater than or equal to {@code start}
    */
   private ThreeSmooth(long start) {
     long nextRow = 1;
-    columns.add(nextRow);
+    rows.add(nextRow);
     int index = 0;
     long nextColumn = 1;
-    while (columns.get(columns.size()-1) < start) {
+    while (rows.get(rows.size()-1) < start) {
       while (nextRow < start) {
         nextRow *= 2;
-        columns.set(index, nextRow);
+        rows.set(index, nextRow);
       }
       index++;
       nextColumn *= 3;
       nextRow = nextColumn;
-      columns.add(nextColumn);
+      rows.add(nextColumn);
     }
   }
   
-  private List<Long> columns = new ArrayList<>();
-  boolean limitReached = false;
+  private final List<Long> rows = new ArrayList<>();
+  private boolean limitReached = false;
 
   @Override
   public boolean hasNext() {
@@ -83,7 +86,7 @@ public final class ThreeSmooth implements Iterator<Long> {
     if (limitReached) {
       throw new IllegalStateException("Limit exceeded");
     }
-    Iterator<Long> itr = columns.iterator();
+    Iterator<Long> itr = rows.iterator();
     int index = 0;
     int lowIndex = 0;
     long lowest = itr.next();
@@ -96,36 +99,34 @@ public final class ThreeSmooth implements Iterator<Long> {
         lowIndex = index;
       }
     }
-    if ((lowIndex+1) == columns.size()) {
+    if ((lowIndex+1) == rows.size()) {
       // nextCandidate is now the last element read
       final long next3Power = nextCandidate * 3L;
       // The maximum value of the entire sequence will always be a power of two, but we max out on the 3 powers first.
       if (next3Power < 0) {
-        columns.add(Long.MAX_VALUE);
+        rows.add(Long.MAX_VALUE);
       } else {
-        columns.add(next3Power);
+        rows.add(next3Power);
       }
     }
-    long next = columns.get(lowIndex);
+    long next = rows.get(lowIndex);
     long newNext = next * 2L;
     if (newNext < next) { // test for overflow (newNext will be negative)
       limitReached = true;
     }
-    columns.set(lowIndex, next*2L);
+    rows.set(lowIndex, next*2L);
     return next;
   }
   
   private static void printExp(int base, int exp) {
-    if (exp > 0) {
-      switch (exp) {
-        case 1:
-          System.out.print(base);
-          break;
-        case 0:
-          break;
-        default:
-          System.out.printf("%d^%d", base, exp);
-      }
+    switch (exp) {
+      case 1:
+        System.out.print(base);
+        break;
+      case 0:
+        break;
+      default:
+        System.out.printf("%d^%d", base, exp);
     }
   }
 
