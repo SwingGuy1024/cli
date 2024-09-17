f82b6f89e89d87037d0535127b8799bb63187098
SwingGuy1024 <SwingGuy1024@yahoo.com>
2024-09-16 17:43:41 -0700

    Tools to help crack Wordle -- Fixed warnings in LSort

--- Tools/src/com/neptunedreams/tools/cli/LSort.java
+++ Tools/src/com/neptunedreams/tools/cli/LSort.java
@@ -4,7 +4,6 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
