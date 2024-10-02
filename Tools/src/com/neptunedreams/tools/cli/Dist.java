9b1897e29c332f1b6a27748ff9e4aed70d4e8fa9
SwingGuy1024 <SwingGuy1024@yahoo.com>
2024-10-02 15:56:00 -0700

    Now does the work of WSort, so we don't need to pipe it. Uses real Wordle characters

--- Tools/src/com/neptunedreams/tools/cli/Dist.java
+++ Tools/src/com/neptunedreams/tools/cli/Dist.java
@@ -1,11 +1,13 @@
+
 // use: dist
 package com.neptunedreams.tools.cli;
 
-import java.awt.Toolkit;
-import java.awt.datatransfer.StringSelection;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * <p>Created by IntelliJ IDEA.</p>
@@ -15,37 +17,29 @@ import java.util.List;
  */
 public enum Dist {
   ;
+  private static final String GREEN = "\uD83D\uDFE9";
+  private static final String YELLOW = "\uD83D\uDFE8";
+  private static final String DARKGRAY = "⬛"; // 0x2B1B (dark gray box)
 
   public static void main(String[] args) {
     String master = args[0].toUpperCase();
-    List<String> candidates = List.of(Arrays.copyOfRange(args, 1, args.length));
-    List<Duple> duples = match(master, candidates);
-    StringBuilder builder = new StringBuilder();
-    for (Duple duple : duples) {
-      builder
-          .append(duple.pattern())
-          .append(' ');
-    }
-    String result = builder.toString();
-    StringSelection ss = new StringSelection(result);
-    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, ss);
-    for (String s: candidates) {
-      System.err.print(s + ' ');
-    }
-    
-    // Print the result to both System.err and System.out, because we assume the output will be piped to WSort,
-    // but we still want to see it..
-    System.err.println();
-    System.err.println(result);
-    System.out.println(result);
+    List<String> candidates = new LinkedList<>(List.of(Arrays.copyOfRange(args, 1, args.length)));
+    candidates.replaceAll(String::toUpperCase);
+    Map<String, List<String>> duples = match(master, candidates);
+    wordSort(duples);
   }
 
-  private static List<Duple> match(String master, List<String> candidates) {
-    List<Duple> result = new LinkedList<>();
+  private static Map<String, List<String>> match(String master, List<String> candidates) {
+    TreeMap<String, List<String>> result = new TreeMap<>();
     for (String c : candidates) {
-      result.add(new Duple(master, matchWord(c, master)));
+      result.merge(matchWord(c, master), new LinkedList<>(List.of(c)), Dist::reMap);
     }
     return result;
+  }
+  
+  private static <T> List<T> reMap(List<T> fullList, List<T> newEntries) {
+    fullList.addAll(newEntries);
+    return fullList;
   }
 
   private static String matchWord(String candidate, String master) {
@@ -77,17 +71,42 @@ public enum Dist {
         }
       }
     }
-    return new String(pattern);
+    StringBuilder builder = new StringBuilder();
+    for (char c: pattern) {
+      switch (c) {
+        case '.' -> builder.append(DARKGRAY);
+        case '/' -> builder.append(YELLOW);
+        case 'X' -> builder.append(GREEN);
+        default -> throw new IllegalStateException("Unexpected value: " + c);
+      }
+    }
+    return builder.toString();
   }
-
+  
   private static int indexOf(char[] array, char c) {
-    for (int i = 0; i < array.length; ++i) {
+    for (int i = 0; i < (array.length); ++i) {
       if (array[i] == c) {
         return i;
       }
     }
     return -1;
   }
-
-  record Duple(String word, String pattern) { }
+  
+  private static void wordSort(Map<String, List<String>> map) {
+    List<Integer> countList = new LinkedList<>();
+    for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+      final List<String> matches = entry.getValue();
+      for (String match: matches) {
+        System.out.printf("%s %s%n", entry.getKey(), match); // NON-NLS
+      }
+      System.out.println(); // NON-NLS
+      countList.add(matches.size());
+    }
+    countList.sort(Collections.reverseOrder());
+    for (int i: countList) {
+      System.out.print(i);
+      System.out.print('.');
+    }
+    System.out.println();
+  }
 }
