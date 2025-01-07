c4495d96ec3c0d50298d9d183772d08ed84af83c
SwingGuy1024 <SwingGuy1024@yahoo.com>
2025-01-07 02:31:31 -0800

    Better sorting. Made lots of things final.

--- Tools/src/com/neptunedreams/tools/cli/Dist.java
+++ Tools/src/com/neptunedreams/tools/cli/Dist.java
@@ -4,6 +4,7 @@ package com.neptunedreams.tools.cli;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -21,35 +22,35 @@ public enum Dist {
   private static final String YELLOW = "\uD83D\uDFE8";
   private static final String DARKGRAY = "⬛"; // 0x2B1B (dark gray box)
 
-  public static void main(String[] args) {
-    String master = args[0].toUpperCase();
-    List<String> candidates = new LinkedList<>(List.of(Arrays.copyOfRange(args, 1, args.length)));
+  public static void main(final String[] args) {
+    final String master = args[0].toUpperCase();
+    final List<String> candidates = new LinkedList<>(List.of(Arrays.copyOfRange(args, 1, args.length)));
     candidates.replaceAll(String::toUpperCase);
-    Map<String, List<String>> duples = match(master, candidates);
+    final Map<Result, List<String>> duples = match(master, candidates);
     wordSort(duples);
   }
 
-  private static Map<String, List<String>> match(String master, List<String> candidates) {
-    TreeMap<String, List<String>> result = new TreeMap<>();
-    for (String c : candidates) {
+  private static Map<Result, List<String>> match(final String master, final List<String> candidates) {
+    final TreeMap<Result, List<String>> result = new TreeMap<>();
+    for (final String c : candidates) {
       result.merge(matchWord(c, master), new LinkedList<>(List.of(c)), Dist::reMap);
     }
     return result;
   }
   
-  private static <T> List<T> reMap(List<T> fullList, List<T> newEntries) {
+  private static <T> List<T> reMap(final List<T> fullList, final List<T> newEntries) {
     fullList.addAll(newEntries);
     return fullList;
   }
 
-  private static String matchWord(String candidate, String master) {
+  private static Result matchWord(final String candidate, final String master) {
     if (candidate.length() != master.length()) {
       throw new IllegalStateException(String.format("Mismatching lengths: %d != %d for (%s, %s)",
           candidate.length(), master.length(), candidate, master));
     }
-    char[] pattern = ".....".toCharArray();
-    char[] mChars = master.toUpperCase().toCharArray();
-    char[] cChars = candidate.toCharArray();
+    final char[] pattern = ".....".toCharArray();
+    final char[] mChars = master.toUpperCase().toCharArray();
+    final char[] cChars = candidate.toCharArray();
 
     // Find Green matches 
     for (int i = 0; i < pattern.length; ++i) {
@@ -62,28 +63,35 @@ public enum Dist {
 
     // find Yellow Matches
     for (int i = 0; i < pattern.length; ++i) {
-      char c = mChars[i];
+      final char c = mChars[i];
       if (c != '.') {
-        int index = indexOf(cChars, c);
+        final int index = indexOf(cChars, c);
         if (index >= 0) {
           pattern[i] = '/';
           cChars[index] = '.';
         }
       }
     }
-    StringBuilder builder = new StringBuilder();
-    for (char c: pattern) {
+    int score = 0;
+    final StringBuilder builder = new StringBuilder();
+    for (final char c: pattern) {
       switch (c) {
         case '.' -> builder.append(DARKGRAY);
-        case '/' -> builder.append(YELLOW);
-        case 'X' -> builder.append(GREEN);
+        case '/' -> { 
+          score += 11;
+          builder.append(YELLOW);
+        }
+        case 'X' -> {
+          score += 20;
+          builder.append(GREEN);
+        }
         default -> throw new IllegalStateException("Unexpected value: " + c);
       }
     }
-    return builder.toString();
+    return new Result(builder.toString(), score);
   }
   
-  private static int indexOf(char[] array, char c) {
+  private static int indexOf(final char[] array, final char c) {
     for (int i = 0; i < (array.length); ++i) {
       if (array[i] == c) {
         return i;
@@ -92,21 +100,57 @@ public enum Dist {
     return -1;
   }
   
-  private static void wordSort(Map<String, List<String>> map) {
-    List<Integer> countList = new LinkedList<>();
-    for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+  private static void wordSort(final Map<Result, List<String>> map) {
+    final List<Integer> countList = new LinkedList<>();
+    for (final Map.Entry<Result, List<String>> entry : map.entrySet()) {
       final List<String> matches = entry.getValue();
-      for (String match: matches) {
+      for (final String match: matches) {
         System.out.printf("%s %s%n", entry.getKey(), match); // NON-NLS
       }
       System.out.println(); // NON-NLS
       countList.add(matches.size());
     }
     countList.sort(Collections.reverseOrder());
-    for (int i: countList) {
+    for (final int i: countList) {
       System.out.print(i);
       System.out.print('.');
     }
     System.out.println();
+  }
+  
+  private static class Result implements Comparable<Result> {
+    private final String word;
+    private final int score;
+
+    private final Comparator<Result> comparator = 
+        Comparator
+            .<Result>comparingInt(r -> r.score)
+            .thenComparing(r-> r.word);
+
+    Result(final String s, final int score) {
+      word = s;
+      this.score = score;
+    }
+
+    @Override
+    public int hashCode() {
+      return word.hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+      return (obj instanceof Result) && ((Result) obj).word.equals(word);
+    }
+    
+    @Override
+    public int compareTo(final Dist.Result o) {
+      return comparator.compare(this, o);
+//      return (Integer.compare(score, o.score);
+    }
+
+    @Override
+    public String toString() {
+      return word;
+    }
   }
 }
