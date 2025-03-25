d5d13beab938fd63049178ee94f927895198b0b4
SwingGuy1024 <SwingGuy1024@yahoo.com>
2025-03-25 10:38:03 -0700

    Fixed printing of diagnostics. Fixed missing finals

--- Tools/src/com/neptunedreams/tools/cli/wFilter.java
+++ Tools/src/com/neptunedreams/tools/cli/wFilter.java
@@ -25,42 +25,43 @@ public enum wFilter {
   public static final String WWW_ROCKPAPERSHOTGUN_COM = "www.rockpapershotgun.com";
   public static final String FILE = "wordle-past-answers";
 
-  public static void main(String[] args) throws IOException {
+  public static void main(final String[] args) throws IOException {
     if (args.length > 0) {
       filter(args);
     }
-  }
+    System.exit(0); // Avoids bug where VM prints out diagnostics.
+  } // End of main(String[])
   
-  private static void filter(String[] args) throws IOException {
-    Set<String> archive = getArchive();
-    StringBuilder builder = new StringBuilder();
-    for (String candidate: args) {
+  private static void filter(final String[] args) throws IOException {
+    final Set<String> archive = getArchive();
+    final StringBuilder builder = new StringBuilder();
+    for (final String candidate: args) {
       if (!archive.contains(candidate.toUpperCase())) {
         builder.append(candidate).append(' ');
       }
     }
     System.out.println(builder);
-    Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-    StringSelection stringSelection = new StringSelection(builder.toString());
+    final Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    final StringSelection stringSelection = new StringSelection(builder.toString());
     clipboard.setContents(stringSelection, stringSelection);
   }
   
   private static Set<String> getArchive() throws IOException {
-    URL url = new URL("https", WWW_ROCKPAPERSHOTGUN_COM, FILE);
-    URLConnection connection = url.openConnection();
+    final URL url = new URL("https", WWW_ROCKPAPERSHOTGUN_COM, FILE);
+    final URLConnection connection = url.openConnection();
     connection.connect();
-    Set<String> archive = new HashSet<>();
-    try (BufferedReader ir = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+    final Set<String> archive = new HashSet<>();
+    try (final BufferedReader ir = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
       boolean searching = true;
       while(searching) {
-        String line = ir.readLine();
+        final String line = ir.readLine();
         if ("<ul class=\"inline\">".equals(line)) {
           searching = false;
         }
       }
       boolean processing = true;
       while (processing) {
-        String word = ir.readLine().trim();
+        final String word = ir.readLine().trim();
         if (word.startsWith("<li>")) {
           archive.add(clean(word)); // NON-NLS
         } else {
@@ -71,9 +72,9 @@ public enum wFilter {
     return archive;
   }
   
-  private static String clean(String word) {
-    int start = word.indexOf('>') + 1;
-    int end = word.lastIndexOf('<');
+  private static String clean(final String word) {
+    final int start = word.indexOf('>') + 1;
+    final int end = word.lastIndexOf('<');
     return word.substring(start, end);
   }
 }
