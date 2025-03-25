030842e4a7ee922dd459bf18ebd9eec3f4cd37b0
SwingGuy1024 <SwingGuy1024@yahoo.com>
2025-03-25 03:08:20 -0700

    chekc args Docs

--- Tools/src/com/neptunedreams/tools/cli/WSort.java
+++ Tools/src/com/neptunedreams/tools/cli/WSort.java
@@ -1,27 +1,56 @@
-// use: wSort
+// ue: wSort
 package com.neptunedreams.tools.cli;
 
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.TreeSet;
-import java.util.function.BiFunction;
 
 /**
- * <p>Created by IntelliJ IDEA.</p>
- * <p>Date: 7/3/24</p>
- * <p>Time: 9:43 AM</p>
- * <p>@author Miguel Muñoz</p>
+ * <p>Sorts words, then prints them out in alphabetical order & upper case, with a blank line
+ * whenever the word changes; then prints out the sizes of the word groups in descending order.</p>
+ * <p>For example: If the user enters this:</p>
+ * <pre>
+ *   wSort a b c b d B d B e b
+ * </pre>
+ * <p>the output will look like this:</p>
+ * <pre>
+ *   A
+ *
+ *   B
+ *   B
+ *   B
+ *   B
+ *   B
+ *  
+ *   C
+ *  
+ *   D
+ *   D
+ *  
+ *   E
+ *   52111
+ * </pre>
+ * <p>Created by IntelliJ IDEA.
+ * <br>Date: 7/3/24
+ * <br>Time: 9:43 AM
+ * <br>@author Miguel Muñoz</p>
  */
 public enum WSort {
   ;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    if (args.length == 0) {
+      args = toString(System.in).split(" ");
+    }
     List<String> sortedWords = new LinkedList<>(List.of(args));
     sortedWords.replaceAll(String::toUpperCase);
     Map<String, Integer> wordCount = new TreeMap<>();
@@ -46,5 +75,15 @@ public enum WSort {
     StringSelection ss = new StringSelection(builder.toString());
     Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, ss);
     System.out.println();
+  }
+
+  @SuppressWarnings("SameParameterValue")
+  private static String toString(InputStream in) throws IOException {
+    try (InputStreamReader reader = new InputStreamReader(in);
+        StringWriter 
+            writer = new StringWriter()) {
+      reader.transferTo(writer);
+      return writer.toString().trim();
+    }
   }
 }
